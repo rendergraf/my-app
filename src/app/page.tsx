756162237fc0cbc93e5f5defae082e7e22d71311
Victor Araque <victoraraque267@gmail.com>
2024-08-21 12:00:16 +0200

    cambios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,9 +47,9 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            Docs <span>-&gt;</span>
+            Documentos <span>-&gt;</span>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <p>papapapaap</p>
         </a>
 
         <a
